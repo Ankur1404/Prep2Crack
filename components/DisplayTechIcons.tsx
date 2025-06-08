12f9de3d5ea5d640867a3b94c11fec8cdcf4f4ad
Ankur Kumar <ankurkumarmr@gmail.com>
2025-06-08 13:13:56 +0530

    image fetch correction

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,25 +1,30 @@
 import Image from "next/image";
 
-import {  getTechLogos } from "@/lib/utils";
+import { cn, getTechLogos } from "@/lib/utils";
 
 const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
   const techIcons = await getTechLogos(techStack);
 
   return (
-   <div className="flex flex-row">
-      {techIcons.slice(0, 3).map(({ tech, url, doc }) => (
-        <a
+    <div className="flex flex-row">
+      {techIcons.slice(0, 3).map(({ tech, url }, index) => (
+        <div
           key={tech}
-          href={doc}
-          target="_blank"
-
-          className="relative group bg-dark-300 rounded-full p-2 flex-center"
+          className={cn(
+            "relative group bg-dark-300 rounded-full p-2 flex flex-center",
+            index >= 1 && "-ml-3"
+          )}
         >
-          <div className="absolute -top-8 left-1/2 -translate-x-1/2 whitespace-nowrap bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 z-10">
-            {tech}
-          </div>
-          <Image src={url} width={100} height={100} alt={tech} className="size-4" />
-        </a>
+          <span className="tech-tooltip">{tech}</span>
+
+          <Image
+            src={url}
+            alt={tech}
+            width={100}
+            height={100}
+            className="size-5"
+          />
+        </div>
       ))}
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,119 +2,46 @@ import { interviewCovers, mappings } from "@/constants";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Utility to merge Tailwind classes with clsx
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// Base URL for tech icons
 const techIconBaseURL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons";
 
-const docLinks: Record<string, string> = {
-  react: "https://react.dev/",
-  nextjs: "https://nextjs.org/docs",
-  vuejs: "https://vuejs.org/",
-  express: "https://expressjs.com/",
-  nodejs: "https://nodejs.org/en/docs",
-  mongodb: "https://www.mongodb.com/docs/",
-  mongoose: "https://mongoosejs.com/docs/",
-  mysql: "https://dev.mysql.com/doc/",
-  postgresql: "https://www.postgresql.org/docs/",
-  sqlite: "https://www.sqlite.org/docs.html",
-  firebase: "https://firebase.google.com/docs",
-  docker: "https://docs.docker.com/",
-  kubernetes: "https://kubernetes.io/docs/",
-  aws: "https://docs.aws.amazon.com/",
-  azure: "https://learn.microsoft.com/en-us/azure/",
-  gcp: "https://cloud.google.com/docs",
-  digitalocean: "https://docs.digitalocean.com/",
-  heroku: "https://devcenter.heroku.com/",
-  photoshop: "https://helpx.adobe.com/photoshop/user-guide.html",
-  html5: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-  css3: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-  sass: "https://sass-lang.com/documentation/",
-  tailwindcss: "https://tailwindcss.com/docs",
-  bootstrap: "https://getbootstrap.com/docs/",
-  jquery: "https://api.jquery.com/",
-  typescript: "https://www.typescriptlang.org/docs/",
-  javascript: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-  angular: "https://angular.io/docs",
-  ember: "https://guides.emberjs.com/",
-  backbone: "https://backbonejs.org/",
-  nestjs: "https://docs.nestjs.com/",
-  graphql: "https://graphql.org/learn/",
-  apollo: "https://www.apollographql.com/docs/",
-  webpack: "https://webpack.js.org/concepts/",
-  babel: "https://babeljs.io/docs/en/",
-  rollup: "https://rollupjs.org/",
-  parcel: "https://parceljs.org/docs/",
-  npm: "https://docs.npmjs.com/",
-  yarn: "https://classic.yarnpkg.com/en/docs/",
-  git: "https://git-scm.com/doc",
-  github: "https://docs.github.com/en",
-  gitlab: "https://docs.gitlab.com/",
-  bitbucket: "https://support.atlassian.com/bitbucket-cloud/docs/",
-  figma: "https://help.figma.com/hc/en-us",
-  prisma: "https://www.prisma.io/docs",
-  redux: "https://redux.js.org/introduction/getting-started",
-  flux: "https://facebook.github.io/flux/docs/in-depth-overview/",
-  redis: "https://redis.io/docs/",
-  selenium: "https://www.selenium.dev/documentation/",
-  cypress: "https://docs.cypress.io/",
-  jest: "https://jestjs.io/docs/getting-started",
-  mocha: "https://mochajs.org/",
-  chai: "https://www.chaijs.com/guide/",
-  karma: "https://karma-runner.github.io/latest/index.html",
-  vuex: "https://vuex.vuejs.org/",
-  nuxt: "https://nuxt.com/docs",
-  strapi: "https://docs.strapi.io/",
-  wordpress: "https://developer.wordpress.org/",
-  contentful: "https://www.contentful.com/developers/docs/",
-  netlify: "https://docs.netlify.com/",
-  vercel: "https://vercel.com/docs",
-  amplify: "https://docs.amplify.aws/",
-};
-
-// Normalize tech names to match devicon filenames
 const normalizeTechName = (tech: string) => {
   const key = tech.toLowerCase().replace(/\.js$/, "").replace(/\s+/g, "");
   return mappings[key as keyof typeof mappings];
 };
 
-// Check if icon exists (using HEAD request)
 const checkIconExists = async (url: string) => {
   try {
     const response = await fetch(url, { method: "HEAD" });
-    return response.ok;
+    return response.ok; // Returns true if the icon exists
   } catch {
     return false;
   }
 };
 
-// Get actual URLs for tech logos, fall back to default if not found
 export const getTechLogos = async (techArray: string[]) => {
   const logoURLs = techArray.map((tech) => {
     const normalized = normalizeTechName(tech);
     return {
       tech,
       url: `${techIconBaseURL}/${normalized}/${normalized}-original.svg`,
-      doc: docLinks[normalized] ?? "#",
     };
   });
 
   const results = await Promise.all(
-    logoURLs.map(async ({ tech, url,doc }) => ({
+    logoURLs.map(async ({ tech, url }) => ({
       tech,
-      url: (await checkIconExists(url)) ? url : "/tech.svg",doc,
-      
+      url: (await checkIconExists(url)) ? url : "/tech.svg",
     }))
   );
 
   return results;
 };
 
-
 export const getRandomInterviewCover = () => {
   const randomIndex = Math.floor(Math.random() * interviewCovers.length);
-  return `/covers/${interviewCovers[randomIndex]}`;
+  return `/covers${interviewCovers[randomIndex]}`;
 };
